6a2573e60327f00b85644dc68a33917b50be424f
Chris Chandler <squanderingtime@gmail.com>
2009-05-10 21:26:39 -0700

    Major updates

--- rtree.erl
+++ rtree.erl
@@ -29,30 +29,24 @@
 %% 
 %% This code is available as Open Source Software under the MIT license.
 %% 
-%% Updates at http://github.com/cchandler/meson/
 
 -module(rtree).
 
-% -behaviour(gen_server).
-% -behaviour(gen_mod).
 
-%export for gen_mod
 % -export([start_link/2, start/2, stop/0]).
 % -export([start_link/2]).
 -export([new_tree/0]).
 -export([insert/2]).
--export([choose_leaf/1]).
--export([all_combinations/2]).
-	
-%-define(SERVER, goal-finder).
-%-define(PROCNAME, goal_finder).
+% -export([choose_leaf/1]).
+% -export([all_combinations/2]).
 
-%-record(state, {host}).
 -record(rtree, {root, 
 				min_node_entries,
 				max_node_entries}).
 
--record(node, {values=[], children=[], parent}).
+-record(node, {boundingbox, values=[], children=[], parent}).
+
+-record(boundingbox, {topleft,topright,bottomleft,bottomright, area}).
 
 % leaf node
 % {I (n-dimensional figure), tuple-identifier}
@@ -70,18 +64,20 @@
 % 5. The root node has at least two children unless it is a leaf 
 % 6. All leaves appear on the same level. That means the tree is balanced
 
+%% Generate a new basic tree
 new_tree() -> #rtree{root= #node{}, min_node_entries=1, max_node_entries=3}.
 
-insert(RTree, {}) -> ok;
+
+%% Insert data into the tree
+insert(_RTree, {}) -> ok;
 insert(#rtree{root=RTreeRoot}=RTree, {Figure, Data}=NewRecord) -> 
 	Node = choose_leaf(RTreeRoot),
 	case has_space(RTree, Node) of
 		true -> 
-			NewValues = lists:append(Node#node.values, [NewRecord]),
+			NewValues = lists:append(Node#node.values, [Figure]),
 			update_node(RTree,Node,NewValues);
 		false -> 
-			quadratic_split(lists:append(Node#node.values, [NewRecord])),
-			requires_split
+			quadratic_split(lists:append(Node#node.values, [Figure]))
 	end.
 	
 %% Clean up the RTree
@@ -89,6 +85,7 @@ update_node(RTree,Node,NewValues) ->
 	NewNode = Node#node{values=NewValues},
 	NewRTree = RTree#rtree{root=NewNode}.
 
+%% Determine if the node in question is a leaf node
 is_leaf(Node) -> 
 	if length(Node#node.children) > 0 ->
 		false;
@@ -96,13 +93,30 @@ is_leaf(Node) ->
 		true
 	end.
 	
+%% Split a leaf node that has exceeded max_node_entries using
+%% the quadratic method
 quadratic_split(Values) -> 
-	quadratic_pick_seeds(Values),
-	ok.
+	{Seed1,Seed2} = quadratic_pick_seeds(Values).
 
+%% Choose two elements from the max_node_entries+1 value list
+%% by finding the most "wasteful" bounding box of two antipodal figures
 quadratic_pick_seeds(Values) -> 
-	CombinationValues = all_combinations(Values, []).
+	CombinationValues = all_combinations(Values, []),
+	{_, {_, FinalBoundingBox, Seeds} } = lists:mapfoldl(
+	fun({Figure1,Figure2}=Elem, {D,_BB,Pair}=Acc) -> 
+		BoundingBox = generate_bounding_box(Elem),
+		Figure1Area = figure_area(Figure1),
+		Figure2Area = figure_area(Figure2),
+		if BoundingBox#boundingbox.area - (Figure1Area - Figure2Area) > D ->
+			{Elem, {BoundingBox#boundingbox.area,BoundingBox, {Figure1,Figure2}}};
+		true ->
+			{Elem, Acc}
+		end
+	end, {0,#boundingbox{}, {} }, CombinationValues),
+	io:format("Seeds = ~p BoundingBox = ~p ~n", [Seeds, FinalBoundingBox]),
+	Seeds.
 
+%% Generate all combinations of list pairs
 all_combinations([], Combinations) -> Combinations;
 all_combinations([H | T], Combinations) ->
 	NewCombinations = lists:map(fun(Elem) -> 
@@ -110,10 +124,108 @@ all_combinations([H | T], Combinations) ->
 		end, T),
 	all_combinations(T, NewCombinations ++ Combinations).
 
-generate_bounding_box(Figure1, Figure2) -> ok.
+%%  Find the Area created by the two dimensional rectangle described by Figure
+figure_area(Figure1) ->
+	F1rm = rightmost_point(Figure1),
+	F1tm = topmost_point(Figure1),
+	F1lm = leftmost_point(Figure1),
+	F1bm = bottommost_point(Figure1),
+	{_F1tm_x,F1tm_y} = F1tm,
+	{F1lm_x,_F1lm_y} = F1lm,
+	{_F1bm_x,F1bm_y} = F1bm,
+	{F1rm_x,_F1rm_y} = F1rm,
+	Figure1Area = abs(F1lm_x - F1rm_x) * abs(F1tm_y - F1bm_y).
 
+%% Generate a bounding box around two rectangular figures
+generate_bounding_box({Figure1, Figure2}) -> 
+	% {Figure1, Figure2} = H,
+	
+	io:format("Figure1, Figure2 = ~p ~p ~n", [Figure1, Figure2]),
+	F1rm = rightmost_point(Figure1),
+	F2rm = rightmost_point(Figure2),
+	F1tm = topmost_point(Figure1),
+	F2tm = topmost_point(Figure2),
+	
+	if F1tm > F2tm ->
+		{_,TopCoordinate} = F1tm;
+	true ->
+		{_,TopCoordinate} = F2tm
+	end,
+	
+	if F1rm > F2rm ->
+		{RightCoordinate,_} = F1rm;
+	true ->
+	    {RightCoordinate,_} = F2rm
+	end,
+	F1lm = leftmost_point(Figure1),
+	F2lm = leftmost_point(Figure2),
+	F1bm = bottommost_point(Figure1),
+	F2bm = bottommost_point(Figure2),
+	
+	if F1bm < F2bm ->
+		{_,BottomCoordinate} = F1bm;
+	true ->
+		{_,BottomCoordinate} = F2bm
+	end,
+	
+	if F1lm < F2lm ->
+		{LeftCoordinate,_} = F1lm;
+	true ->
+		{LeftCoordinate,_} = F2lm
+	end,
+	
+	TopEdgeLength = abs(LeftCoordinate - RightCoordinate),
+	SideEdgeLength = abs(TopCoordinate - BottomCoordinate),
+	BBArea = TopEdgeLength * SideEdgeLength,
+	BoundingBox = #boundingbox{ area=BBArea, 
+	    topleft={LeftCoordinate,TopCoordinate},
+	    topright={RightCoordinate, TopCoordinate}, 
+	    bottomright={RightCoordinate, BottomCoordinate}, 
+	    bottomleft={LeftCoordinate, BottomCoordinate} },
+	
+	BoundingBox.	
+	
+%% Find the right-most point of a two dimensional figure
+rightmost_point({Point1,Point2}=_Figure) -> 
+	{X1,Y1} = Point1,
+	{X2,Y2} = Point2,
+	if X1 > X2 ->
+		Point1;
+	true ->
+		Point2
+	end.
 
+%% Find the left-most point of a two dimensional figure	
+leftmost_point({Point1,Point2}=_Figure) -> 
+	{X1,Y1} = Point1,
+	{X2,Y2} = Point2,
+	if X1 < X2 ->
+		Point1;
+	true ->
+		Point2
+	end.
 
+%% Find the top-most point of a two dimensional figure
+topmost_point({Point1,Point2}=_Figure) -> 
+	{X1,Y1} = Point1,
+	{X2,Y2} = Point2,
+	if Y1 > Y2 ->
+		Point1;
+	true ->
+		Point2
+	end.
+
+%% Find the bottom-most point of a two dimensional figure
+bottommost_point({Point1,Point2}=_Figure) -> 
+	{X1,Y1} = Point1,
+	{X2,Y2} = Point2,
+	if Y1 < Y2 ->
+		Point1;
+	true ->
+		Point2
+	end.
+
+%% Determine if a given leaf node has space for another value
 has_space(RTree,Node) ->
 	Max = RTree#rtree.max_node_entries,
 	Values = Node#node.values,
@@ -124,6 +236,7 @@ has_space(RTree,Node) ->
 		%Requires a new node
 	end.
 
+%% Choose a leaf for adding a new value to on insert
 choose_leaf(Node) -> 
 	case is_leaf(Node) of
 		true -> Node;
